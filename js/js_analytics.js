9bd500ae983d5b2cbbfdb182594a41a04951a8ef
a.delduca <a.delduca@reply.it>
2020-12-16 15:06:43 +0100

    Form layout updates. Remove debug logs.

--- js/js_analytics.js
+++ js/js_analytics.js
@@ -3,7 +3,7 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -22,7 +22,6 @@ async function initFingerprintJS() {
     };
     ga.l = +new Date;
 
-    console.log(visitorId);
     ga('create', 'UA-180475860-1', {
         'storage': 'none',
         'clientId': visitorId
